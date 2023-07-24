0c862042e9cae68bf863d469866797c6419f31f2
bashman1 <wamulabash1@gmail.com>
2023-07-24 21:52:36 +0300

    worked on the colors

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -30,13 +30,13 @@ button{
     font-size: 1.1.rem;
     cursor: pointer;
     padding: 1rem 2rem;
-    border: 3px solid #23d997;
+    border: 3px solid #530f02;
     background: transparent;
     color: white;
     transition: all 0.5s ease;
 
         &:hover{
-            background-color: #23d997;
+            background-color: #530f02;
             color: white;
         }
 }
@@ -66,7 +66,7 @@ line-height: 1.5;
     }
     span{
         font-weight: bold;
-        color:#23d997;
+        color:#530f02;
     }
     p{
         font-size: 1.4rem;
