ebcb872cdae0f43c3df5b2ff0d4f4288acbae3dd
akoraSolomon <0701901774aka@gmail.com>
2023-07-26 00:37:03 +0300

    contact us down

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const base_url = "http://virce.co.ug/core/api/public-web-content";
+export const base_url = "https://virce.co.ug/core/api/public-web-content";
 export const path = "/";
 // export const path = "/tambisa/";
 
